5b95794603799893ca2d7c60e477581fdc41f4a9
aryansikariya7741 <139913185+aryansikariya7741@users.noreply.github.com>
2025-03-22 22:01:01 +0530

    remainingfeatures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/dashboard";
 import Game from "./pages/Game";
+import InvestmentTracker from "./pages/InvestmenGuide";
+import FinanceEducator from "./pages/FinanceEducator";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<AuthPage />} />
         <Route path="/game" element={<Game />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/investment-guide" element={<InvestmentTracker />} />
+        <Route path="/finance-educator" element={<FinanceEducator />} />
       </Routes>
     </Router>
   );
